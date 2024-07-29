141b9bd0c036c441345ea9d7c0d788fb07c025a2
Vladyslav-Markevych <vladen035@gmail.com>
2024-07-29 18:29:42 +0300

    hw_50 retake

--- hw_50/app.js
+++ hw_50/app.js
@@ -1,4 +1,30 @@
-import { write, read } from "./os-module.js";
+import { systemInfo } from "./os-module.js";
+import { writeFile, readFile } from "fs";
 
-write();
-read();
+function write(item) {
+  writeFile("result.txt", item, "utf8", (err) => {
+    if (err) {
+      console.error("Error:", err);
+    } else {
+      console.log("Saved");
+    }
+  });
+}
+
+function read() {
+  readFile("result.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+function startApp() {
+  const dataInfo = systemInfo();
+  write(dataInfo);
+  read();
+}
+
+startApp();
--- hw_50/os-module.js
+++ hw_50/os-module.js
@@ -1,28 +1,15 @@
 import os from "os";
-import { writeFile, readFile } from "fs";
 
-export function write() {
-  const systemInfo = `
-    Platform: ${os.platform()}
-    Arch: ${os.arch()}
-    Number of CPUs: ${os.cpus().length}
-    Total Memory: ${(os.totalmem() / 1024 ** 3).toFixed(2)} GB
-    Free Memory: ${(os.freemem() / 1024 ** 3).toFixed(2)} GB
-    `;
-
-  writeFile("result.txt", systemInfo, "utf8", (err) => {
-    if (err) {
-      console.error("Error:", err);
-    } else {
-      console.log("Saved");
-    }
-  });
+function convertByteToGB(byte) {
+  return (byte / 1024 ** 3).toFixed(2);
 }
 
-export function read() {
-  readFile("result.txt", "utf-8", (err, data) => {
-    if (err) console.log(err);
-
-    console.log(data);
-  });
-}
+export const systemInfo = () => {
+  return `
+Platform: ${os.platform()}
+Arch: ${os.arch()}
+Number of CPUs: ${os.cpus().length}
+Total Memory: ${convertByteToGB(os.totalmem())} GB
+Free Memory: ${convertByteToGB(os.freemem())} GB
+`;
+};
